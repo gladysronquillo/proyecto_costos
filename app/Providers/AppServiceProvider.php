f5dda7753d1f93dc93a7be1a6441f7b1668fa1d5
Gladys Ronquillo <gmronquillo@rutatec.com>
2018-05-18 17:53:24 -0500

    Tabla Costos V1.1
    Subo migración de tabla Costos y excel
    composer require maatwebsite/excel
    
    php artisan make:controller CostosController
     php artisan make:model Costos -m

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Se define para evitar error al momento de migrar
+        Schema::defaultStringLength(191);
     }
 
     /**
